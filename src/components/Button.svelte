31d7409e9ceae22e0f787a5bdb297fdebb0de513
won <lk8969@gmail.com>
2023-04-15 22:59:44 +0900

    Feat: app 기본 button 컴포넌트 추가

---
+++ src/components/Button.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	let buttonId: string = "";
+	let className: string = "";
+	export let theme: "dark" | "light" = "dark";
+	export { buttonId as id };
+	export { className as class };
+</script>
+
+<button id={buttonId} class={`${theme} ${className}`} {...$$restProps}>
+	<slot />
+</button>
+
+<style lang="scss">
+	button {
+		border-radius: 10px;
+		color: var(--app-white);
+		font-size: 0.8rem;
+		transition: background-color 0.3s;
+
+		&.dark {
+			background-color: var(--app-dark-green50);
+		}
+
+		&.dark:hover {
+			background-color: var(--app-dark-green);
+		}
+	}
+</style>
