cf15b2d000e7fb5919a3efadb759df4f6157a21e
won <lk8969@gmail.com>
2023-04-15 23:00:08 +0900

    Feat: app 기본 input 추가

---
+++ src/components/Input.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	let className: string = "";
+
+	export { className as class };
+	export let value: string = "";
+</script>
+
+<input class={className} bind:value {...$$restProps} />
+
+<style lang="scss">
+	input {
+		border-radius: 5px;
+		background-color: var(--app-white);
+
+		&:focus::placeholder {
+			opacity: 0;
+		}
+	}
+</style>
