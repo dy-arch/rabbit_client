ada3cce22729f1d294ebbf0ce43ac7b843e84448
won <lk8969@gmail.com>
2023-04-16 17:25:43 +0900

    Refact: accessToken이 있을 경우에만 user 정보 fetching 하도록 변경

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,16 +4,18 @@ import remote from "./api";
 export const handle = (async ({ event, resolve }) => {
 	const accessToken = event.cookies.get("access_token");
 
-	const { data, status } = await remote.get("http://localhost:4000/api/auth", {
-		headers: { Authorization: accessToken },
-	});
+	if (accessToken) {
+		const { data, status } = await remote.get("http://localhost:4000/api/auth", {
+			headers: { Authorization: accessToken },
+		});
 
-	if (status === 200) {
-		event.locals.user = data;
-	}
+		if (status === 200) {
+			event.locals.user = data;
+		}
 
-	if (status === 401 || status === 500) {
-		event.locals.user = null;
+		if (status === 401 || status === 500) {
+			event.locals.user = null;
+		}
 	}
 
 	return await resolve(event);
