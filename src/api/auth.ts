6e68a65f26db06bc176197555c3afaa2b5b569d0
won <lk8969@gmail.com>
2023-05-21 20:57:06 +0900

    Feat: 사용자 정보 조회 api, token refresh api 생성

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,20 +1,35 @@
-import remote from "./";
+import { browser } from "$app/environment";
+import remote from ".";
 import Cookies from "js-cookie";
 
-interface User {
+export interface User {
 	id: string;
 	name: string;
 	role: "USER" | "ADMIN";
-}
-
-interface LoginResponse extends User {
 	isAllowed: boolean;
-	accessToken: string;
 }
 
 interface LoginRequest {
 	id: string;
 	password: string;
+}
+
+interface LoginResponse extends User {
+	accessToken: string;
+}
+
+export interface TokenExpiredPayload {
+	status: number;
+	name: "UnAuthorizedError";
+	message: "UnAuthorized";
+	payload: {
+		isExpiredToken: boolean;
+	};
+}
+
+interface RefreshResponse {
+	accessToken: string;
+	refreshToken: string;
 }
 
 export async function login(loginConf: LoginRequest) {
@@ -38,13 +53,41 @@ export async function login(loginConf: LoginRequest) {
 		};
 	}
 
-	Cookies.set("access_token", data.accessToken, { path: "/", secure: true });
+	const expireTime = Date.now() + 7200000 - 600000; // 2시간 - 10분
+	Cookies.set("access_token", data.accessToken, {
+		path: "/",
+		secure: true,
+		expires: new Date(expireTime),
+	});
 
 	return { data, status, message: null };
 }
 
-export async function getUserState() {
-	const { data, status } = await remote.get<User>("/api/auth/");
+export async function getUserState(accessToken?: string) {
+	const getUserPath = accessToken ? "http://localhost:4000/api/auth/" : "/api/auth/";
+	const config = accessToken
+		? {
+				headers: { Authorization: accessToken },
+		  }
+		: undefined;
+
+	const { data, status } = await remote.get<User | TokenExpiredPayload>(getUserPath, config);
+
+	return {
+		data,
+		status,
+	};
+}
+
+export async function refresh(refreshToken?: string) {
+	const refreshUrl = refreshToken ? "http://localhost:4000/api/auth/refresh" : "/api/auth/refresh";
+
+	const { data, status } = await remote.post<RefreshResponse>(refreshUrl, undefined, {
+		headers: {
+			"Content-Type": "text/plain",
+			Cookie: `refresh_token=${refreshToken}`,
+		},
+	});
 
 	return {
 		data,
