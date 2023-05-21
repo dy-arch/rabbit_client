f9699d8413e3969f23995c183077b2e77e9d424d
won <lk8969@gmail.com>
2023-05-21 20:59:01 +0900

    Feat: response error 시 refresh 이후 재시도 추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,18 @@
+import { browser } from "$app/environment";
 import axios, { AxiosError, type AxiosResponse, type InternalAxiosRequestConfig } from "axios";
 import Cookies from "js-cookie";
 
-const remote = axios.create({ withCredentials: true });
+const remote = axios.create({
+	headers: {
+		"Content-Type": "application/json",
+	},
+	withCredentials: true,
+});
 
 const requestHandler = (conf: InternalAxiosRequestConfig) => {
+	const accessToken = Cookies.get("access_token");
+
 	if (!conf.headers.Authorization) {
-		const accessToken = Cookies.get("access_token");
 		conf.headers.Authorization = accessToken;
 	}
 
@@ -22,8 +29,37 @@ const responseHandler = (res: AxiosResponse) => {
 	return res;
 };
 
-const responseErrorHandler = (err: AxiosError) => {
-	return err.response;
+const responseErrorHandler = async (err: AxiosError) => {
+	// 서버 api 조회 시 예외처리
+	if (!browser) {
+		return err.response;
+	}
+
+	const { data, status } = err.response as AxiosResponse<{
+		payload?: { isExpiredToken: boolean };
+	}>;
+	// access token 만료 시 refresh
+	if (status === 401 && data?.payload?.isExpiredToken) {
+		const expireTime = Date.now() + 7200000 - 600000; // 2시간 - 10분
+
+		const { data, status } = await remote.post<{ accessToken: string }>("/api/auth/refresh");
+
+		if (status === 200) {
+			Cookies.set("access_token", data.accessToken, {
+				path: "/",
+				secure: true,
+				expires: new Date(expireTime),
+			});
+
+			const res = await remote.request(err.config!);
+			return {
+				data: res.data,
+				status: res.status,
+			};
+		}
+	}
+
+	return { data, status };
 };
 
 remote.interceptors.response.use(responseHandler, responseErrorHandler);
