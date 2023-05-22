83ab2f75521b3c8fb9e65a2c6261b4d86e41d2cd
won <lk8969@gmail.com>
2023-05-22 22:01:28 +0900

    Feat: 접속 시 사용자 session 검증 interval 추가

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,47 @@
 <script lang="ts">
-	import { onMount } from "svelte";
+	import { onDestroy, onMount } from "svelte";
 	import { user } from "../store/user";
 	import type { LayoutData } from "./$types";
 	import "./global.scss";
 	import "./reset.scss";
+	import Cookies from "js-cookie";
+	import { goto } from "$app/navigation";
+	import { page } from "$app/stores";
 
 	export let data: LayoutData;
 
+	let pathname = $page.url.pathname;
+
+	let userValidateInterval: NodeJS.Timer | null = null;
+
+	function setUserValidateInterval() {
+		if (pathname === "/auth/login") return;
+
+		userValidateInterval = setInterval(() => {
+			const unsubscribe = user.subscribe((user) => {
+				if (!user) {
+					goto("/auth/login");
+				}
+			});
+			unsubscribe();
+		}, 10000);
+	}
+
+	function clearUserValidateInterval() {
+		if (userValidateInterval) {
+			clearInterval(userValidateInterval);
+		}
+	}
+
 	onMount(async () => {
 		const { user: userInfo } = data;
-
 		user.set(userInfo);
+
+		setUserValidateInterval();
+	});
+
+	onDestroy(() => {
+		clearUserValidateInterval();
 	});
 </script>
 
