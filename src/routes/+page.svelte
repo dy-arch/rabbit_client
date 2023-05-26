93da8be1abe74c5a935fa944d821ce7057461e45
won <lk8969@gmail.com>
2023-05-26 23:14:25 +0900

    Feat: 사이드 토글 메뉴 추가

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,16 @@
 <script lang="ts">
+	import HambergerButton from "../components/HambergerButton.svelte";
+
+	let sideActive: boolean = false;
+	$: sideClass = sideActive ? "active" : "";
+
+	function hambergerClickHandler() {
+		if (sideActive) {
+			sideActive = false;
+		} else {
+			sideActive = true;
+		}
+	}
 </script>
 
 <svelte:head>
@@ -6,8 +18,33 @@
 </svelte:head>
 
 <main id="mainPage">
+	<aside id="sideMenu" class={sideClass}>
+		<HambergerButton class="expandButton" active={sideActive} on:click={hambergerClickHandler} />
+	</aside>
 	<a href="/auth/login">로그인페이지</a>
 </main>
 
 <style lang="scss">
+	#mainPage {
+		display: flex;
+		width: 100%;
+		height: 100vh;
+	}
+
+	#sideMenu {
+		position: relative;
+		width: 2.5rem;
+		background: var(--app-dark-green);
+		transition: width 0.4s ease-in-out;
+
+		&.active {
+			width: 20%;
+		}
+	}
+
+	:global(.expandButton) {
+		position: absolute;
+		top: 0.6rem;
+		right: 0.65rem;
+	}
 </style>
